55f4203250c1ee5da6c6cd4bcde544d8755fca1f
Fasakin Henry <fasakinhenry@gmail.com>
2024-06-26 23:12:04 +0100

    :wastebasket: chore: update gitignore file
    Updated the .gitignore file to include new patterns.
    
    0.0.3

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingLayout from "./layouts/LandingLayout";
 import AuthLayout from "./layouts/AuthLayout";
-import DashboardLayout from "./layouts/DashboardLayout";
-import LandingPage from "./pages/Home";
+import AppLayout from "./layouts/AppLayout";
 import Features from "./pages/Functions";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import Dashboard from "./pages/Dashboard";
+import Home from './components/Home';
+import Education from './components/Education';
+import Locator from './components/Locator';
+import Schedule from './components/Schedule';
+import Rewards from './components/Rewards';
+import Messages from './components/Messages';
+import Profile from './components/Profile';
 
 const App = () => (
   <Router>
@@ -15,16 +21,23 @@ const App = () => (
       {/* Landing page Layout */}
       <Route path="/" element={<LandingLayout />}>
         <Route index element={<LandingPage />} />
-        <Route path="/functions" element={<Features />} />
+        <Route path="functions" element={<Features />} />
       </Route>
       {/* Signin and Signup page layout */}
       <Route element={<AuthLayout />}>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignupPage />} />
       </Route>
       {/* Main application or dashboard layout */}
-      <Route element={<DashboardLayout />}>
-        <Route path="/dashboard" element={<Dashboard />} />
+      <Route element={<AppLayout />}>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="home" element={<Home />} />
+        <Route path="education" element={<Education />} />
+        <Route path="locator" element={<Locator />} />
+        <Route path="schedule" element={<Schedule />} />
+        <Route path="rewards" element={<Rewards />} />
+        <Route path="messages" element={<Messages />} />
+        <Route path="profile" element={<Profile />} />
       </Route>
     </Routes>
   </Router>
